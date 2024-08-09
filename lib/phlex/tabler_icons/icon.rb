9a00ce887a6bdd62e4faa9e5703ce5baa62202b8
Elvinas Predkelis <elvinas.predkelis@gmail.com>
2024-08-09 13:39:17 +0300

    removing attr_reader from the icon base

--- lib/phlex/tabler_icons/icon.rb
+++ lib/phlex/tabler_icons/icon.rb
@@ -3,8 +3,6 @@ require "phlex"
 module Phlex
   module TablerIcons
     class Icon < Phlex::SVG
-      attr_reader :class, :height, :width, :stroke_width
-
       # TODO Remove options[:classes] in future versions
       def initialize(**options)
         @class = options[:class] || options[:classes] || Phlex::TablerIcons.configuration.default_class # :classes are for backward compatibility - will be removed in future versions
